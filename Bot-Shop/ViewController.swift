d42b10d1d5bd95123b00f2adcae2914dce62cccb
MakeRafa <rafavazquez229@gmail.com>
2021-02-27 17:33:13 -0800

    init project

---
+++ Bot-Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot-Shop
+//
+//  Created by Rafa Vazquez on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
