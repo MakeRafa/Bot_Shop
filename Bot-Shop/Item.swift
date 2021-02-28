b5d95232ad356011a0d7ac1221c61e644f0ab410
MakeRafa <rafavazquez229@gmail.com>
2021-02-27 18:23:35 -0800

    Users can see items for sale in collection view

---
+++ Bot-Shop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Bot-Shop
+//
+//  Created by Rafa Vazquez on 2/27/21.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
--- Bot-Shop/NewOrderViewController.swift
+++ Bot-Shop/NewOrderViewController.swift
@@ -9,6 +9,15 @@ import Foundation
 import UIKit
 
 class NewOrderViewController: UIViewController {
+    
+    let data = [
+        Item(title: "Respiratory", image: UIImage(named: "robot1")!),
+        Item(title: "Muscular", image: UIImage(named: "robot2")!),
+        Item(title: "Endocrine", image: UIImage(named: "robot3")!),
+        Item(title: "Excretory", image: UIImage(named: "robot4")!),
+        Item(title: "Lymphatic", image: UIImage(named: "robot5")!),
+        Item(title: "Nervous", image: UIImage(named: "robot6")!)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +25,7 @@ class NewOrderViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         // Do any additional setup after loading the view.
-        view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+//        view.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
         setViews()
     }
     
@@ -45,17 +54,19 @@ class NewOrderViewController: UIViewController {
 
 extension NewOrderViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
         cell.backgroundColor = .green
+        cell.data = self.data[indexPath.row]
+
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.height/5)
+        return CGSize(width: collectionView.frame.width/2.1, height: collectionView.frame.height/3)
     }
     
 }
