f505ccef95a95793e2c092ebc5e0d8dfb4e0f5ce
MakeRafa <rafavazquez229@gmail.com>
2021-02-28 13:14:35 -0800

    Built out basic table view

--- Bot-Shop/PastOrderViewController.swift
+++ Bot-Shop/PastOrderViewController.swift
@@ -9,22 +9,41 @@ import UIKit
 
 class PastOrderViewController: UIViewController {
 
+    let tableView =  UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = #colorLiteral(red: 0, green: 0.416482389, blue: 0, alpha: 1)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(PastOrderCell.self, forCellReuseIdentifier: "cell")
+        
+        setUpTableView()
+    }
+    func setUpTableView(){
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
 
+}
+
+extension PastOrderViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PastOrderCell
+            cell.accessoryType = .disclosureIndicator
+            cell.selectionStyle = .none
+        return cell
     }
-    */
-
+    
+    
 }
